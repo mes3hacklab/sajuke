8207744015675ad41641b087c3953e08248937c2
Federico klez Culloca <federicoculloca>
2014-10-05 15:53:43 +0200

    click on album to see its tracklist

--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,64 @@
 $( document ).ready(function() {
-			$( "#autocomplete" ).on( "listviewbeforefilter", function ( e, data ) {
-				var ul = $( this ),
-					input = $( data.input ),
-					value = input.val(),
-					html = "";
-				ul.html( "" );
-				if ( value && value.length > 1 ) {
-					ul.html( "" );
-					ul.listview( "refresh" );
-					$.ajax({
-						url: "http://localhost:8080/api/basicSearch",
-						dataType: "json",
-						crossDomain: true,
-						data: {
-							query: input.val()
-						}
-					})
-					.then( function ( response ) {
-					    html += "<li data-role='list-divider'>Authors</li>";
-                        $.each( response.authors, function ( i, val ) {
+    $( "#autocomplete" ).on( "listviewbeforefilter", function ( e, data ) {
+        var ul = $( this ),
+        input = $( data.input ),
+        value = input.val(),
+            html = "";
+        ul.html( "" );
+        if ( value && value.length > 1 ) {
+            ul.html( "" );
+            ul.listview( "refresh" );
+            $.ajax({
+                url: "http://localhost:8080/api/basicSearch",
+                dataType: "json",
+                crossDomain: true,
+                data: {
+                    query: input.val()
+                }
+            })
+            .then( function ( response ) {
+                html += "<li data-role='list-divider'>Authors</li>";
+                $.each( response.authors, function ( i, val ) {
+                    html += "<li class='authors elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'>" + val.name + "</li>";
+                });
+                html += "<li data-role='list-divider'>Songs</li>";
+                $.each( response.songs, function ( i, val ) {
+                    html += "<li class='songs elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.name + "</h2><p><strong> Album: </strong>" + val.album.name + "<strong> Author: </strong>" + val.author.name + "</p></li>";
+                });
+                html += "<li data-role='list-divider'>Albums</li>";
+                $.each( response.albums, function ( i, val ) {
+                    html += "<li class='albums elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2>" + val.name + "</h2><p><strong> Author: </strong>" + val.author.name + " </p><input type='hidden' value='" + val.id + "'/></li>";
+                });
+                ul.html( html );
+                ul.listview( "refresh" );
+                ul.trigger( "updatelayout");
+            });
+        }
+    });
+    $('#autocomplete').on('click', '.albums', function () {
+        albumid = $(this).find('input').val();
+        var ul = $('#autocomplete');
+        var html = '';
+        ul.html("");
 
-                            html += "<li class='authors elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'>" + val.name + "</li>";
-                        });
-					    html += "<li data-role='list-divider'>Songs</li>";
-						$.each( response.songs, function ( i, val ) {
-                           html += "<li class='songs elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.name + "</h2><p><strong> Album: </strong>" + val.album.name + "<strong> Author: </strong>" + val.author.name + "</p></li>";
-                        });
-					    html += "<li data-role='list-divider'>Albums</li>";
-                        $.each( response.albums, function ( i, val ) {
-                            html += "<li class='albums elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2>" + val.name + "</h2><p><strong> Author: </strong>" + val.author.name + " </p></li>";
-                         });
-						ul.html( html );
-						ul.listview( "refresh" );
-						ul.trigger( "updatelayout");
-					});
-
-				}
-			});
+        $.ajax({
+            url: "http://localhost:8080/api/songs",
+            dataType: "json",
+            crossDomain: true,
+            data: {
+                album:  albumid
+            }
+        })
+        .then( function (response) {
+            html += "<li data-role='list-divider'>Songs</li>";
+            $.each( response, function ( i, val ) {
+                html += "<li class='songs elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.name + "</h2><p><strong> Album: </strong>" + val.album.name + "<strong> Author: </strong>" + val.author.name + "</p></li>";
+            });
+            ul.html( html );
+            ul.listview( "refresh" );
+            ul.trigger( "updatelayout");
+        });
+    });
 
 });
+

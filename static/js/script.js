b272f1d823dd4c6ac0281fa901a6620aa5d96859
Mattia Campanelli <MattiaCampanelli>
2014-10-06 13:37:10 +0200

    ricerca per authors, cambio nome pagina

--- static/js/script.js
+++ static/js/script.js
@@ -19,7 +19,7 @@ $( document ).ready(function() {
             .then( function ( response ) {
                 html += "<li data-role='list-divider'>Authors</li>";
                 $.each( response.authors, function ( i, val ) {
-                    html += "<li class='authors elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'>" + val.name + "</li>";
+                    html += "<li class='authors elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'>" + val.name + "<input type='hidden' value='" + val.id + "'/></li>";
                 });
                 html += "<li data-role='list-divider'>Songs</li>";
                 $.each( response.songs, function ( i, val ) {
@@ -50,6 +50,34 @@ $( document ).ready(function() {
             }
         })
         .then( function (response) {
+            html += "<li data-role='list-divider'>Songs</li>";
+            $.each( response, function ( i, val ) {
+                html += "<li class='songs elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.name + "</h2><p><strong> Album: </strong>" + val.album.name + "<strong> Author: </strong>" + val.author.name + "</p></li>";
+            });
+            ul.html( html );
+            ul.listview( "refresh" );
+            ul.trigger( "updatelayout");
+        });
+    });
+    $('#autocomplete').on('click', '.authors', function () {
+        authorid = $(this).find('input').val();
+        var ul = $('#autocomplete');
+        var html = '';
+        ul.html("");
+
+        $.ajax({
+            url: "http://localhost:8080/api/songs",
+            dataType: "json",
+            crossDomain: true,
+            data: {
+                author:  authorid
+            }
+        })
+        .then( function (response) {
+            html += "<li data-role='list-divider'>Albums</li>";
+            $.each( response, function ( i, val ) {
+                html += "<li class='albums elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.album.name + "</h2><p><strong> Author: </strong>" + val.author.name + "</p><input type='hidden' value='" + val.id + "'/></li>";
+            });
             html += "<li data-role='list-divider'>Songs</li>";
             $.each( response, function ( i, val ) {
                 html += "<li class='songs elem ui-btn ui-btn-icon-right ui-icon-carat-r ui-li ui-li-has-thumb' data-icon='arrow-r' data-iconpos='right'><img src='img/box.png'><h2> " + val.name + "</h2><p><strong> Album: </strong>" + val.album.name + "<strong> Author: </strong>" + val.author.name + "</p></li>";
